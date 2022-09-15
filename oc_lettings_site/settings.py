5f0d10ce400355c76e2a91551f3da5449e4f405a
loic5015 <loic.hernio@gmail.com>
2022-09-15 15:06:21 +0200

    install sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,24 @@
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://bb44d4d9d0044ec4b72b887f7d58ddf0@o1410721.ingest.sentry.io/6748242",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,6 +4,10 @@ from lettings import views as lettings_view
 from profiles import views as profiles_view
 from .views import index
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('', index, name='index'),
     path('lettings/', lettings_view.index, name='lettings_index'),
@@ -11,4 +15,7 @@ urlpatterns = [
     path('profiles/', profiles_view.index, name='profiles_index'),
     path('profiles/<str:username>/', profiles_view.profile, name='profile'),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
+
+
