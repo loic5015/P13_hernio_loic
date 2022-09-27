3c1d337cf79b1d2b41a6dd9e1c6f3ecc85122b80
loic5015 <loic.hernio@gmail.com>
2022-09-27 18:11:08 +0200

    modif dockerfile et config.yml

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -137,4 +137,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
