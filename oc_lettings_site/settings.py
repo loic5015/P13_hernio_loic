d8f8258a9b7f1eaa5f03ccb44bdcfd87b6882dcc
loic5015 <loic.hernio@gmail.com>
2022-09-27 18:32:08 +0200

    compresssion whitenoise

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -138,4 +138,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
